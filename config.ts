f3d94abe02d2b946b3e5ab4c2f807bd57e252a38
Muhammed Taskiran <Muhammed Taskiran>
2017-03-04 23:30:38 +0100

    added config file

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 var config = {
-    apiKey: "AIzaSyAtAD7pd1CLkMOqOX0RioZRsTrBFj-pZDM",
-    authDomain: "jugendhackt-korea.firebaseapp.com",
-    databaseURL: "https://jugendhackt-korea.firebaseio.com",
-    storageBucket: "jugendhackt-korea.appspot.com",
-    messagingSenderId: "1035809417809"
+    apiKey: "[APIKEY]",
+    authDomain: "[DOMAIN]",
+    databaseURL: "[DBURL]",
+    storageBucket: "[STORAGEBUCKET]",
+    messagingSenderId: "[SENDERID]"
   };
