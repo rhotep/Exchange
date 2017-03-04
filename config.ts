5b856f0318a2925c12e597c3c6c611216b9cb1fa
Muhammed Taskiran <Muhammed Taskiran>
2017-03-04 23:29:50 +0100

    added config file

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 var config = {
-    apiKey: "[APIKEY]",
-    authDomain: "[DOMAIN]",
-    databaseURL: "[DBURL]",
-    storageBucket: "[STORAGEBUCKET]",
-    messagingSenderId: "[SENDERID]"
+    apiKey: "AIzaSyAtAD7pd1CLkMOqOX0RioZRsTrBFj-pZDM",
+    authDomain: "jugendhackt-korea.firebaseapp.com",
+    databaseURL: "https://jugendhackt-korea.firebaseio.com",
+    storageBucket: "jugendhackt-korea.appspot.com",
+    messagingSenderId: "1035809417809"
   };
